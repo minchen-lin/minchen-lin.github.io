d4b1ef3d7c2c4f6a6db6bff526741d1ef7d84302
minchenlin <augua1216@gmail.com>
2016-02-04 22:37:05 +0800

    smooth scroll

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,10 @@
-/**
- * Created by joyce on 2016/1/24.
- */
+$(function () {
+    $("a[href^=#]").on("click", function(e) {
+        var targetElement = $(this.hash);
+        $('html,body').animate({
+            scrollTop: targetElement.offset().top
+        }, 800);  /* 0.8秒的速度 */
+        e.preventDefault();  /* 拿掉預設的會執行的動作 */
+        history.pushState({}, "", this.href);  /* 上一頁是回到上一個狀況 */
+    });
+});
