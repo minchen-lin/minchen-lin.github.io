ae145ba8854e912d5aebb03a4f60099d6b7a0447
Minchen Lin <augua1216+dev@gmail.com>
2018-08-19 17:26:48 +0800

    change cover, about page and add Illustration page

--- assets/js/index.js
+++ assets/js/index.js
@@ -69,6 +69,9 @@ $(function () {
 
     });
 
+    $('.sticker-bg').on('click', function() {
+        window.open('/illustration.html')
+    });
 
     //$('.overlayTW').on('click', function(event){
     //    if( $(event.target).is('.close') || $(event.target).is('.overlayTW') ) {
@@ -85,7 +88,33 @@ $(function () {
     //    }
     //    $('body').css('overflow', 'scroll');
     //});
+
+    $(document).ready(function(){
+        var stars=800;
+        var $stars=$(".stars");
+        var r=800;
+        for(var i=0;i<stars;i++){
+          var $star=$("<div/>").addClass("star");
+          $stars.append($star);
+        }
+        $(".star").each(function(){
+          var cur=$(this);
+          var s=0.2+(Math.random()*1);
+          var curR=r+(Math.random()*300);
+          cur.css({ 
+            transformOrigin:"0 0 "+curR+"px",
+            transform:" translate3d(0,0,-"+curR+"px) rotateY("+(Math.random()*360)+"deg) rotateX("+(Math.random()*-50)+"deg) scale("+s+","+s+")"
+             
+          })
+        })
+      })
+
+
 });
+
+// stars animation 
+
+
 
 //window.onscroll = function() {scrollTosec()};
 //
