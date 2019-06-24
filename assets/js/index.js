fb863d8a342f9383853e130bb8a7a3860d374f40
Minchen Lin <augua1216+dev@gmail.com>
2019-06-25 01:08:06 +0200

    enlarge wireframe testing

--- assets/js/index.js
+++ assets/js/index.js
@@ -202,6 +202,17 @@ $(function () {
         };
     });
 
-
+    //enlarge image
+    $('.wireframe').on('click', function () {
+        $('.zoom-in')
+            .css({
+                'backgroundImage': `url(${this.src})`,
+                'z-index': 10000
+            })
+            .addClass('open')
+            .one('click', function () {
+                $(this).removeClass('open');
+            })
+    });
 
 });
